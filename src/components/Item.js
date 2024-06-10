2e7c186d5d80fa5ce0ff6c55b127f797fdd257fb
booneytan <73635818+booneytan@users.noreply.github.com>
2024-06-10 12:33:39 +0800

    Add media queries

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,17 @@
 export default function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
-      <input type="checkbox" value={item.packed} onChange={() => onToggleItem(item.id)} />
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity} {item.description}
-      </span>
+      <input
+        type="checkbox"
+        value={item.packed}
+        id={item.id}
+        onChange={() => onToggleItem(item.id)}
+      />
+      <label htmlFor={item.id}>
+        <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+          {item.quantity} {item.description}
+        </span>
+      </label>
       <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
